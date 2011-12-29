c965686424ac519af1ba41059a27aaf8ae17d2ee
Mike DeLaurentis <delaurentis@gmail.com>
2011-12-29 09:37:37 -0500

    Use regexes to extract stuff from markdown file, rather than tokenizing first.

--- lowbrew.py
+++ lowbrew.py
@@ -2,6 +2,9 @@
 
 import sys
 import re
+import argparse
+import unittest
+
 from itertools import takewhile 
 from collections import namedtuple
 
@@ -14,22 +17,16 @@ class Header:
         return "%s\n%s" % (self.name, len(self.name) * "-")
 
 class Weight:
-    def __init__(self, number, unit):
-        self.number = number
+    def __init__(self, numerator, denominator, unit):
+        d = 1 if denominator is None else denominator
+        self.number = numerator / d
         self.unit = unit
 
     def __str__(self):
         return "%s %s" % (self.pounds(), "lbs")
 
     def pounds(self):
-        (t, s) = self.number
-
-        if self.unit.kind in ["LB", "LBS"]:
-            if t in ["INT", "FLOAT"]:
-                return float(s)
-            if t == "RATIONAL":
-                (n, d) = s.split("/")
-                return float(n) / float(d)
+        return self.number
 
 class Grain:
 
@@ -39,7 +36,7 @@ class Grain:
         self.mashed = mashed
 
     def __str__(self):
-        return "%s %s" % (self.weight, " ".join([x[1] for x in self.name]))
+        return "%s %s" % (self.weight, self.name)
 
 def categorize_token(token):
 
@@ -83,6 +80,49 @@ def complement(f):
 
 Token = namedtuple('Token', ['kind', 'text'])
 
+def parse_name(text):
+    print "Searching"
+    m = re.search("(.*)\n=+", text)    
+    return m.group(1)
+
+def section_end_pos(text, name):
+    
+    section_re = re.compile("^" + name + "\n-+$", re.I|re.MULTILINE)
+    section = section_re.search(text)
+    return section.end()
+
+
+def next_section_start_pos(text, start):
+    """Returns the position of the start of the next level 1 or 2
+    header following the given start position, or the length of the
+    text if there are no more headers."""
+    section_re = re.compile("^.*\n-+$", re.I|re.MULTILINE)    
+    next_section = section_re.search(text, start)
+    return len(text) if next_section is None else next_section.start()
+
+def section_limits(text, name):
+    start = section_end_pos(text, "grains")
+    end = next_section_start_pos(text, start)
+    return (start, end)
+
+def parse_grains(text):
+    
+    grain_line_re = re.compile("^(\d+|\d*\.\d+|\d+\.\d*)(/(\d+))?\s+(lb|lbs|oz)\s+(.*)$", re.I|re.MULTILINE)
+
+    (start, end) = section_limits(text, "grains")
+    
+    grains = []
+
+    for m in grain_line_re.finditer(text, start, end):
+
+        (n, d, unit, name) = m.group(1,3,4,5)
+        n = float(n)
+        if d is not None: d = float(d)
+        w = Weight(n, d, unit)
+        g = Grain(name, w)
+        grains.append(g)
+    return grains
+
 class Recipe:
     
     def __init__(self, filename):
@@ -94,13 +134,16 @@ class Recipe:
         self.pre_boil_sg = None
         self.post_boil_sg = None
         self.notes = None
-        
+        self.text = ""
+        with open(filename) as f:
+            text = f.read()
+        f.close()
         if filename is not None:
             self._parse(filename)
 
     def _parse(self, filename):
         tokens = self._tokenize(filename)
-        
+
         while tokens is not None and len(tokens) > 0:
             (grains, tokens) = self._parse_grain_list(tokens)
             if grains is None:
@@ -130,17 +173,6 @@ class Recipe:
             return (Weight(weight, unit), tokens[2:])
         return (None, tokens)
         
-    def _parse_grain(self, tokens):
-        """Attempts to parse a grain specification from the given
-        tokens.  Returns (grain, rest) if it can parse a grain from
-        the beginning of tokens, otherwise (None, tokens)."""
-
-        (weight, tokens) = self._parse_weight(tokens)
-        if weight is not None:
-            name = [x for x in takewhile(is_word_or_number, tokens)]
-            return (Grain(name, weight), tokens[len(name):])
-        return (None, tokens)
-
     def _parse_header(self, tokens):
         if len(tokens) < 3:
             return (None, tokens)
@@ -154,23 +186,6 @@ class Recipe:
             return (Header(name, line), tokens[2:])
         return (None, tokens)
 
-    def _parse_grain_list(self, tokens):
-
-        (header, rest) = self._parse_header(tokens)
-
-        grains = []
-        if header is not None and header.name == "Grains":
-            while (rest is not None and
-                   len(rest) > 0 and
-                   self._parse_header(rest)[0] is None):
-                (g, rest) = self._parse_grain(rest)
-                if g is None:
-                    rest = rest[1:]
-                else:
-                    grains.append(g)
-            return (grains, rest)
-        return (None, tokens)
-
     def __str__(self):
         res = ""
         res += "\n\n"
@@ -179,6 +194,55 @@ class Recipe:
         for g in self.grains:
             res += str(g) + "\n"
         return res
+    
+class LBTest(unittest.TestCase):
+    def test_parse_name(self):
+        name = parse_name("Other Stuff\nOatmeal Stout\n====\n")
+        self.assertEqual(name, "Oatmeal Stout")
 
-recipe = Recipe(sys.argv[1])
-print recipe
+    def test_parse_grains(self):
+        text = """
+Fake Ale
+========
+
+Grains
+------
+
+11 lbs American 2 Row Pale
+1.0 lb Crystal 60
+1. lb Crystal 45
+.5 lb Crystal 120
+1/2 lb Chocolate Malt
+
+Hops
+----
+1 oz Northern Brewer
+1 oz Cascade
+"""
+        grains = parse_grains(text)
+        weights = [g.weight.number for g in grains]
+        names = [g.name for g in grains]
+        expected_names = [
+            "American 2 Row Pale",
+            "Crystal 60",
+            "Crystal 45",
+            "Crystal 120",
+            "Chocolate Malt"]
+        self.assertEquals([11, 1, 1, .5, .5], weights)        
+        self.assertEquals(expected_names, names)
+
+
+def dotest(args):
+    suite = unittest.TestLoader().loadTestsFromTestCase(LBTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+parser = argparse.ArgumentParser(description="Do some stuff")
+subparsers=parser.add_subparsers(help="Sub-command help")
+
+parser_test=subparsers.add_parser("test", help="test")
+parser_test.set_defaults(func=dotest)
+
+args = parser.parse_args()
+args.func(args)
+#recipe = Recipe(sys.argv[1])
+#print recipe
